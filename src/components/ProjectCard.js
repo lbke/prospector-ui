826848765cc57ea642051ae4a05ddb79d2c3ec96
eric-burel <eb@lebrun-burel.com>
2020-01-27 18:41:31 +0100

    fix projectCard wrong copy pasta

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,56 +1,86 @@
-import React from "react";
-import Button from "@material-ui/core/Button";
+import React, { PureComponent } from "react";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+//import CardActionArea from "@material-ui/core/CardActionArea";
+import Typography from "@material-ui/core/Typography";
 import { withStyles } from "@material-ui/core/styles";
-
-import { getFromParam } from "../utils/utils"
+import _delay from 'lodash/delay'
 
 const styles = theme => ({
-    button: {
-        margin: theme.spacing.unit
-    }
+    goodCard: {
+        border: "2px solid green",
+    },
+    card: {
+        cursor: "pointer"
+    },
+    cardContent: {
+        padding: 16,
+        '&:last-child': {
+            padding: 16
+        }
+    },
 });
 
 
-const Actions = ({ scrapProjects, getProjects, classes }) => (
-    <React.Fragment>
-        <Button
-            className={classes.button}
-            variant="contained"
-            color="primary"
-            onClick={() => getProjects(getFromParam(1))}
-        >
-            Reload
-    </Button>
-        <Button
-            className={classes.button}
-            variant="outlined"
-            onClick={() => scrapProjects(getFromParam(1))}
-        >
-            Scrap
-    </Button>
-        <Button
-            className={classes.button}
-            variant="contained"
-            color="primary"
-            onClick={() => getProjects(getFromParam(7))}
-        >
-            Reload week
-    </Button>
-        <Button
-            className={classes.button}
-            variant="outlined"
-            onClick={() => scrapProjects(getFromParam(7))}
-        >
-            Scrap week
-    </Button>
-        <Button
-            className={classes.button}
-            variant="outlined"
-            onClick={() => window.open("https://www.codeur.com", "_blank")}
-        >
-            Open Codeur.com
-    </Button>
-    </React.Fragment>
+const withHover = C => {
+    class HoverProvider extends PureComponent {
+        state = {
+            hover: false,
+        }
+        hover = false
+        onEnter = () => {
+            this.hover = true
+            // change state only if it makes sens
+            _delay(() => {
+                if (this.hover) {
+                    this.setState({ hover: true })
+                }
+            }, 150)
+        }
+        onLeave = () => {
+            this.hover = false
+            this.setState({ hover: false })
+        }
+        render() {
+            return (
+                <div onMouseEnter={this.onEnter} onMouseLeave={this.onLeave}>
+                    <C {...this.state} {...this.props} />
+                </div>
+            )
+        }
+    }
+    return HoverProvider
+}
+
+const BadCard = ({ title, description, onClick, classes, hover }) => (
+    <Card raised={true} onClick={onClick} className={classes.card}>
+        <CardContent className={classes.cardContent}>
+            <Typography variant="body2">{title}</Typography>
+            {hover && <Typography component="p">{description}</Typography>}
+        </CardContent>
+    </Card>
+);
+const DefaultCard = ({ title, description, onClick, classes, hover }) => (
+    <Card raised={true} onClick={onClick} className={classes.card}>
+        <CardContent className={classes.cardContent}>
+            <Typography variant="headline">{title}</Typography>
+            {hover && <Typography component="p">{description}</Typography>}
+        </CardContent>
+    </Card>
+);
+const GoodCard = ({ title, description, onClick, classes, hover }) => (
+    <Card raised={true} onClick={onClick} className={`${classes.goodCard} ${classes.card}`}>
+        <CardContent className={classes.cardContent}>
+            <Typography variant="headline">{title}</Typography>
+            <Typography component="p">{description}</Typography>
+        </CardContent>
+    </Card>
 );
 
-export default withStyles(styles)(Actions);
+const ProjectCard = ({ project, ...otherProps }) => {
+    if (project.score < 50) return <BadCard  {...otherProps}  {...project} />;
+    if (project.score > 50) return <GoodCard  {...otherProps}  {...project} />;
+    return <DefaultCard {...otherProps} {...project} />;
+};
+
+export default withStyles(styles)(withHover(ProjectCard));
