4420fb33adc62baf1201d7b4533604bee35c63a7
eric-burel <eb@lebrun-burel.com>
2020-01-27 17:32:36 +0100

    start to move projects components

---
+++ src/components/ProjectCard.js
@@ -0,0 +1,56 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import { withStyles } from "@material-ui/core/styles";
+
+import { getFromParam } from "./utils"
+
+const styles = theme => ({
+    button: {
+        margin: theme.spacing.unit
+    }
+});
+
+
+const Actions = ({ scrapProjects, getProjects, classes }) => (
+    <React.Fragment>
+        <Button
+            className={classes.button}
+            variant="contained"
+            color="primary"
+            onClick={() => getProjects(getFromParam(1))}
+        >
+            Reload
+    </Button>
+        <Button
+            className={classes.button}
+            variant="outlined"
+            onClick={() => scrapProjects(getFromParam(1))}
+        >
+            Scrap
+    </Button>
+        <Button
+            className={classes.button}
+            variant="contained"
+            color="primary"
+            onClick={() => getProjects(getFromParam(7))}
+        >
+            Reload week
+    </Button>
+        <Button
+            className={classes.button}
+            variant="outlined"
+            onClick={() => scrapProjects(getFromParam(7))}
+        >
+            Scrap week
+    </Button>
+        <Button
+            className={classes.button}
+            variant="outlined"
+            onClick={() => window.open("https://www.codeur.com", "_blank")}
+        >
+            Open Codeur.com
+    </Button>
+    </React.Fragment>
+);
+
+export default withStyles(styles)(Actions);
---
+++ src/pages/projects.js
@@ -0,0 +1,130 @@
+import React, { PureComponent } from "react";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { withStyles } from "@material-ui/core/styles";
+import ProjectCard from "../components/ProjectCard";
+import Actions from "../components/Actions";
+import { getTodayFromParam, humanizeFromParam } from "./utils";
+
+//import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+//import Image from "../components/image"
+import SEO from "../components/seo"
+
+const styles = theme => ({
+    unstyledLink: {
+        "&:link": {
+            textDecoration: "none"
+        }
+    }
+});
+
+const baseUrl = process.env.API_URL
+const scrapUrl = baseUrl + "/query";
+const getUrl = baseUrl + "";
+const withProjects = C => {
+    class ProjectsContainer extends PureComponent {
+        state = {
+            projects: [],
+            scrapingProjects: false,
+            loadingProjects: false,
+            from: getTodayFromParam()
+        };
+        componentDidMount() {
+            this.init();
+        }
+        init = async () => {
+            this.getProjects(getTodayFromParam());
+            //await this.scrapProjects(getTodayFromParam());
+        };
+        scrapProjects = async from => {
+            this.setState({ scrapingProjects: true });
+            const res = await fetch(`${scrapUrl}?from=${from}`).then(res =>
+                res.json()
+            );
+            this.setState({ scrapingProjects: false });
+            this.getProjects(from);
+        };
+        getProjects = async from => {
+            this.setState({ loadingProjects: true });
+            const projects = await fetch(`${getUrl}?from=${from}`).then(res =>
+                res.json()
+            );
+            await this.setState({
+                projects
+            });
+            this.setState({ loadingProjects: false, from });
+            return projects;
+        };
+        render() {
+            return (
+                <C
+                    {...this.props}
+                    {...this.state}
+                    getProjects={this.getProjects}
+                    scrapProjects={this.scrapProjects}
+                />
+            );
+        }
+    }
+    return ProjectsContainer;
+};
+
+const ProjectLink = withStyles(styles)(({ url, children, classes }) => (
+    <a className={classes.unstyledLink} href={url} target="_blank">
+        {children}
+    </a>
+));
+const ProjectsPage = ({
+    getProjects,
+    scrapProjects,
+    scrapingProjects,
+    loadingProjects,
+    projects,
+    from
+}) => (
+        <Layout>
+            <SEO title="Projects" />
+            <Grid container spacing={16}>
+                <Grid item xs={12}>
+                    <Actions getProjects={getProjects} scrapProjects={scrapProjects} />
+                </Grid>
+                {!loadingProjects && (
+                    <Grid item xs={12}>
+                        <Typography>Projets postés depuis {humanizeFromParam(from)}</Typography>
+                    </Grid>
+                )}
+                {scrapingProjects && (
+                    <Grid item xs={12}>
+                        <Typography component="span">Scraping en cours...</Typography>
+                    </Grid>
+                )}
+                {loadingProjects && (
+                    <Grid item xs={12}>
+                        <span>Loading...</span>
+                        <Typography component="span">
+                            Chargement des projets en cours...
+        </Typography>
+                    </Grid>
+                )}
+                <Grid item xs={12}>
+                    <Grid container spacing={8}>
+                        {projects.map(project => (
+                            <Grid item xs={12} sm={11} md={10} key={project.id}>
+                                <ProjectLink url={project.url}>
+                                    <ProjectCard project={project} />
+                                </ProjectLink>
+                            </Grid>
+                        ))}
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Layout>
+    );
+
+const WrappedProjectsPage = withStyles(withProjects(ProjectsPage))(styles);
+
+
+export default WrappedProjectsPage;
